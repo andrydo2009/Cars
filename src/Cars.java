7cb5e74809a00162e2857e7e3e5444e1c018b306
Андрей <andrydo2009@yandex.ru>
2022-12-13 00:38:17 +0300

    Check!

---
+++ src/Cars.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+public class Cars {
+
+    String brand;
+    String model;
+    double engineVolume;
+    String color;
+    int year;
+    String country;
+
+    public Cars() {
+    }
+
+    Cars (String brand, String model, double engineVolume, String color, int year, String country){
+        if (brand==null^ Objects.equals(brand,"")){this.brand="No info";}
+        if (color==null^ Objects.equals(color,"")){this.color="No info";}
+        if (country==null^ Objects.equals(country,"")){this.country="No info";}
+        this.year = Math.max(year, 0);
+        //noinspection ManualMinMaxCalculation
+        if (engineVolume<=0.0) {this.engineVolume=0.0;} else { this.engineVolume=engineVolume; }
+        if (model==null^ Objects.equals(model,"")){this.model="No info";}
+    }
+
+    @Override
+    public String toString() {
+        return brand + " " + model + ", " + year +
+                " год выпуска, сборка " + country +
+                ", цвет кузова " + color + ", обьем двиагателя - " + engineVolume + " литров";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Cars cars_1 = new Cars(
+                "Lada","Granta",1.7,"желтый",2015,"Россия");
+        Cars cars_2 = new Cars(
+                "Audi", "A8 50 L TDI quattro",3.0,"черный",2020,"Германия");
+        Cars cars_3 = new Cars(
+                "BMW","Z8",3.0,"черный",2021,"Германия");
+        Cars cars_4 = new Cars(
+                "KIA","Sportage",2.4,"красный",2018,"Южная Корея");
+        Cars cars_5 = new Cars(
+                "Hyundai","Avante",1.6,"оранжевый",2016,"Южная Корея");
+        Cars cars_6 = new Cars(
+
+        );
+        Cars cars_7 = new Cars("","",0,"",-2011,""
+
+        );
+        System.out.println(cars_1);
+        System.out.println(cars_2);
+        System.out.println(cars_3);
+        System.out.println(cars_4);
+        System.out.println(cars_5);
+        System.out.println(cars_6);
+        System.out.println(cars_7);
+
+
+
+
+
+    }
+}
